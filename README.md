ac5e0f3375e616e79c793c02efa7290b2cd3e163
conkip <ckip.games@gmail.com>
2024-12-11 04:25:28 -0700

    some updates

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
 # Art-Buisness-Website
 very cool
-
-idea to dynamically change painting size:
-1. divide the desired height by the total height
-2. multiply the width and height by that
-
-or
-
-divide width by height to get ratio
-multiply that by desired height to get width
-
-descriptions of art could include size, date, artist, type (ex- acrylic on canvas), and price
-
-
-possibly-
-add sorting the galery based on: (high->low and low->high)
-price
-size
-date
--- testServer.js
+++ testServer.js
@@ -10,13 +10,16 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-const hostname = '127.0.0.1'; //local host for now
+const hostname = '127.0.0.1'; //change to 'leah.knodel.me'
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 const mongoose = require('mongoose');
-const URL = "mongodb://127.0.0.1/myDB";
+const URL = "mongodb://127.0.0.1/projdb"; //change to 'mongodb://leah.knodel.me/projdb'
+
+const User = null;
+const Painting = null;
 
 let username = null;
 
@@ -26,23 +29,52 @@ let username = null;
  */
 async function startServer()
 {
-    await setupDatabase();
+    // database stuff
     await mongoose.connect(URL);
+
+    const PaintingSchema = new mongoose.Schema({
+        name: String,
+        image: String,
+        desc: String,
+        bid: {
+            type: Number,
+            default: 0
+        },
+        bidHolder: {
+            type: String,
+            default: ""
+        },
+        sold: {
+            type: Boolean,
+            default: false
+        },
+    });
+
+    Painting = mongoose.model("Painting", PaintingSchema);
 
     const UserSchema = new mongoose.Schema({
         username: String,
-        fav_paintings: Array,
-        bid_paintings: Array});
-    
-    const User = mongoose.model("User", UserSchema);
+        my_bids: [Painting],
+        my_likes: [Painting]
+    });
+
+    User = mongoose.model("User", UserSchema);
 
     await setupPaintings();
 
-    // Define routes
+
+    
+    // db routes
+
     app.get('/users', async (req, res) => {
-    const users = await User.find(); // Fetch all users
-    res.json(users);
+        const users = await User.find(); // Fetch all users
+        res.json(users);
     });
+
+    app
+
+
+    // cookie routes
 
     app.get("/getCookie", (req,res) => {
         let old_count = req.cookies.username;
@@ -62,37 +94,58 @@ async function startServer()
         res.send("Cookie has been cleared!");
     });
 
-    app.get("/login/:username", (req,res) => {
+
+
+    // login routes
+
+    app.get("/login/:someUsername", async (req,res) => {
         console.log("Request received on URL:", req.url);
-
         res.statusCode = 200;
-        //res.setHeader("Content-Type", "text/plain");
-
-        //serach db for the username
-        username = "username";
-        res.send(username);
+        
+        let user = await User.findOne({username:someUsername})
+        res.send(user);
     });
 
-    app.get("/signup/:username", (req,res) => {
+    app.get("/signup/:someUsername", async(req,res) => {
         console.log("Request received on URL:", req.url);
-
         res.statusCode = 200;
-        //res.setHeader("Content-Type", "text/plain");
 
-        //create new db entry for the username sent
-        //if username exists, send false, else send true
-        successful =true;
-        res.send(successful);
+        let user = await User.findOne({username:someUsername})
+
+        // user exists
+        if(user != null){
+            // sends t/f if successful or not
+            res.send(false)
+        }
+        else{
+            const newUser = new User({
+                username: someUsername
+            });
+    
+            await newUser.save();
+    
+            res.send(true);
+        }
     });
 
     app.get("/getuser", (req,res) => {
         console.log("Request received on URL:", req.url);
-
         res.statusCode = 200;
-        //res.setHeader("Content-Type", "text/plain");
 
-        //send the current user
+        // send the current user
         res.send(username);
+    });
+
+    
+
+    // gallery routes
+
+    app.get("/login/:someUsername", async (req,res) => {
+        console.log("Request received on URL:", req.url);
+        res.statusCode = 200;
+
+        let user = await User.findOne({username:someUsername})
+        res.send(user);
     });
 
     /**
@@ -121,28 +174,17 @@ async function startServer()
 }
 
 async function setupPaintings() {
-    const PaintingSchema = new mongoose.Schema({
-        name: String,
-        image: String,
-        desc: String,
-        bid: Number
-    });
-
-    const Painting = mongoose.model("Painting", PaintingSchema)
-
     let template = new Painting({
         name: "Example",
         image: "Example.jpg",
         desc: "Dimensions (ex- 24x24)\nDate (ex- 2015)\nPaint Type (ex- Oil, acrylic)\n" +
               "Support/Surface (ex- Wood, Fabric)\nFinish (Matte, Glossy)\nFramed(optional)",
-        bid:0
     });
 
     let painting1 = new Painting({
         name: "Beyone The Limit",
         image: "BeyondTheLimit.jpg",
-        desc: "24x24 x 3\n2015\nOil\nWood\nExpoxy coating",
-        bid:0
+        desc: "24x24 x 3\n2015\nOil\nWood\nExpoxy coating"
     });
 
     await painting1.save();
@@ -150,8 +192,7 @@ async function setupPaintings() {
     let painting2 = new Painting({
         name: "Blue Pallete",
         image: "BluePallete.jpg",
-        desc: "36x36\n2015\nOil\nFabric\nTextured",
-        bid:0
+        desc: "36x36\n2015\nOil\nFabric\nTextured"
     });
 
     await painting2.save();
@@ -159,8 +200,7 @@ async function setupPaintings() {
     let painting3 = new Painting({
         name: "Chasing Blues",
         image: "ChasingBlues.jpg",
-        desc: "16x24\n2015\nOil\n_\n_",
-        bid:0
+        desc: "16x24\n2015\nOil\n_\n_"
     });
 
     await painting3.save();
@@ -168,8 +208,7 @@ async function setupPaintings() {
     let painting4 = new Painting({
         name: "Circular Echo",
         image: "CircularEcho.jpg",
-        desc: "24x24\n2015\n_\n_\n_",
-        bid:0
+        desc: "24x24\n2015\n_\n_\n_"
     });
 
     await painting4.save();
@@ -177,8 +216,7 @@ async function setupPaintings() {
     let painting5 = new Painting({
         name: "Colors In Motion",
         image: "ColorsInMotion.jpg",
-        desc: "30x36\n2015\nAcrylic\n_\n_",
-        bid:0
+        desc: "30x36\n2015\nAcrylic\n_\n_"
     });
 
     await painting5.save();
@@ -186,8 +224,7 @@ async function setupPaintings() {
     let painting6 = new Painting({
         name: "Colors Of Liberty",
         image: "ColorsOfLiberty.jpg",
-        desc: "24x30\n2015\n_\n_\n_",
-        bid:0
+        desc: "24x30\n2015\n_\n_\n_"
     });
 
     await painting6.save();
@@ -195,8 +232,7 @@ async function setupPaintings() {
     let painting7 = new Painting({
         name: "Color Spectrum",
         image: "ColorSpectrum.jpg",
-        desc: "36x48\n2015\nAcrylic\nWood\nEpoxy Coating",
-        bid:0
+        desc: "36x48\n2015\nAcrylic\nWood\nEpoxy Coating"
     });
 
     await painting7.save();
@@ -204,8 +240,7 @@ async function setupPaintings() {
     let painting8 = new Painting({
         name: "Cosmic Tides",
         image: "CosmicTides.jpg",
-        desc: "12x18\n2015\nAcrylic\n_\n_",
-        bid:0
+        desc: "12x18\n2015\nAcrylic\n_\n_"
     });
 
     await painting8.save();
@@ -213,8 +248,7 @@ async function setupPaintings() {
     let painting9 = new Painting({
         name: "Dot Fusion",
         image: "DotFusion.jpg",
-        desc: "48x48\n2015\n_\n_\n_",
-        bid:0
+        desc: "48x48\n2015\n_\n_\n_"
     });
 
     await painting9.save();
@@ -222,8 +256,7 @@ async function setupPaintings() {
     let painting10 = new Painting({
         name: "Dot Symphony",
         image: "DotSymphony.jpg",
-        desc: "?x?\n2015\n_\n_\n_",
-        bid:0
+        desc: "_x_\n2015\n_\n_\n_"
     });
 
     await painting10.save();
@@ -231,8 +264,7 @@ async function setupPaintings() {
     let painting11 = new Painting({
         name: "Eternal Light",
         image: "EternalLight.jpg",
-        desc: "36x48\n2015\nOil\n_\n_\nFramed",
-        bid:0
+        desc: "36x48\n2015\nOil\n_\n_\nFramed"
     });
 
     await painting11.save();
@@ -240,8 +272,7 @@ async function setupPaintings() {
     let painting12 = new Painting({
         name: "Eternal Sunshine",
         image: "EternalSunshine.jpg",
-        desc: "36x48\n2015\nAcrylic\n_\n_",
-        bid:0
+        desc: "36x48\n2015\nAcrylic\n_\n_"
     });
 
     await painting12.save();
@@ -249,8 +280,7 @@ async function setupPaintings() {
     let painting13 = new Painting({
         name: "Flowing Essence",
         image: "FlowingEssence.jpg",
-        desc: "6x6 x 3\n2015\nAcrylic\nWood\nEpoxy Coating",
-        bid:0
+        desc: "6x6 x 3\n2015\nAcrylic\nWood\nEpoxy Coating"
     });
 
     await painting13.save();
@@ -258,8 +288,7 @@ async function setupPaintings() {
     let painting14 = new Painting({
         name: "Hazy Drift",
         image: "HazyDrift.jpg",
-        desc: "36x60\n2015\nAcrylic\n_\n_",
-        bid:0
+        desc: "36x60\n2015\nAcrylic\n_\n_"
     });
 
     await painting14.save();
@@ -267,8 +296,7 @@ async function setupPaintings() {
     let painting15 = new Painting({
         name: "Liquid Dreamscapes",
         image: "LiquidDreamscapes.jpg",
-        desc: "?x?\n2015\n_\n_\n_",
-        bid:0
+        desc: "_x_\n2015\n_\n_\n_"
     });
 
     await painting15.save();
@@ -276,8 +304,7 @@ async function setupPaintings() {
     let painting16 = new Painting({
         name: "Liquid Horizons",
         image: "LiquidHorizons.jpg",
-        desc: "48x60\n2015\n_\n_\n_",
-        bid:0
+        desc: "48x60\n2015\n_\n_\n_"
     });
 
     await painting16.save();
@@ -285,8 +312,7 @@ async function setupPaintings() {
     let painting17 = new Painting({
         name: "Oribits In Motion",
         image: "OribitsInMotion.jpg",
-        desc: "8x8 x 4\n2015\n_\nWood\n_",
-        bid:0
+        desc: "8x8 x 4\n2015\n_\nWood\n_"
     });
 
     await painting17.save();
@@ -294,8 +320,7 @@ async function setupPaintings() {
     let painting18 = new Painting({
         name: "Pinwheel",
         image: "Pinwheel.jpg",
-        desc: "36x48\n2015\nAcrylic\n_\n_",
-        bid:0
+        desc: "36x48\n2015\nAcrylic\n_\n_"
     });
 
     await painting18.save();
@@ -303,8 +328,7 @@ async function setupPaintings() {
     let painting19 = new Painting({
         name: "Retro Vibe",
         image: "RetroVibe.jpg",
-        desc: "36x48\n2015\nAcrylic\nWood\nEpoxy coating",
-        bid:0
+        desc: "36x48\n2015\nAcrylic\nWood\nEpoxy coating"
     });
 
     await painting19.save();
@@ -312,8 +336,7 @@ async function setupPaintings() {
     let painting20 = new Painting({
         name: "Spectrum Of The Sea",
         image: "SpectrumOfTheSea.jpg",
-        desc: "18x24\n2015\nAcrylic\n_\n_\nFloating Frame",
-        bid:0
+        desc: "18x24\n2015\nAcrylic\n_\n_\nFloating Frame"
     });
 
     await painting20.save();
@@ -321,8 +344,7 @@ async function setupPaintings() {
     let painting21 = new Painting({
         name: "StrokesOfLight",
         image: "Strokes Of Light.jpg",
-        desc: "18x24\n2015\nOil\n_\n_\nFramed",
-        bid:0
+        desc: "18x24\n2015\nOil\n_\n_\nFramed"
     });
 
     await painting21.save();
@@ -330,8 +352,7 @@ async function setupPaintings() {
     let painting22 = new Painting({
         name: "Sun's Awakening",
         image: "SunsAwakening.jpg",
-        desc: "18x24\n2015\nAcrylic\nWood\nEpoxy Coating\nFramed",
-        bid:0
+        desc: "18x24\n2015\nAcrylic\nWood\nEpoxy Coating\nFramed"
     });
 
     await painting22.save();
